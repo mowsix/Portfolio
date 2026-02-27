a746aa1cdb622d5b941cb83eaf46918febf9e118
Santiago Gallego <santiagoxgallego@gmail.com>
2026-02-27 18:38:37 -0500

    NameLogo

--- src/pages/Home/landing.tsx
+++ src/pages/Home/landing.tsx
@@ -1,46 +1,47 @@
-import React from 'react';
-import { GridScan } from '../../components/GridScan';
-
+import React from "react";
+import { GridScan } from "../../components/GridScan";
+import NameLogo from "../../components/Name/NameLogo";
 
 //import { useNavigate } from 'react-router-dom';
 
-
 const Landing: React.FC = () => {
-    //const navigate = useNavigate();
+  //const navigate = useNavigate();
 
-    return (
-        <div style={{ position: "relative", minHeight: "100vh" }}>
-          <div
-            style={{
-              position: "fixed",
-              inset: 0,
-              zIndex: 0,
-              pointerEvents: "none",
-              width: "100vw",
-              height: "100vh",
-            }}
-          >
-            <GridScan
-              sensitivity={0.55}
-              lineThickness={1}
-              linesColor="#392e4e"
-              lineJitter={0.1}
-              gridScale={0.1}
-              scanColor="#FF9FFC"
-              scanOpacity={0.4}
-              scanGlow={0.5}
-              enablePost
-              bloomIntensity={0.6}
-              chromaticAberration={0.002}
-              noiseIntensity={0.01}
-            />
-          </div>
-    
-          <div style={{ position: "relative", zIndex: 1 }}>
-            {/* tu contenido */}
-          </div>
+  return (
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      <div
+        style={{
+          position: "fixed",
+          inset: 0,
+          zIndex: 0,
+          pointerEvents: "none",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <GridScan
+          sensitivity={0.55}
+          lineThickness={1}
+          linesColor="#392e4e"
+          lineJitter={0.1}
+          gridScale={0.1}
+          scanColor="#FF9FFC"
+          scanOpacity={0.4}
+          scanGlow={0.5}
+          enablePost
+          bloomIntensity={0.6}
+          chromaticAberration={0.002}
+          noiseIntensity={0.01}
+        />
+      </div>
+
+      <div style={{ position: "relative", zIndex: 1 }}>
+        <div className="neon-logo">
+          <NameLogo style={{ width: 520, height: "auto" }} />
         </div>
-      );
-    }
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
